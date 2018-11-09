e539f966400719a13191b06b0d47c5561b3e1c3c
Topi Pohjosaho <topi.pohjosaho@solita.fi>
2018-11-09 08:40:19 +0200

    aloitettu dijkstra

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [clj-http "3.9.1"]]
+                 [clj-http "3.9.1"]
+                 [shams/priority-queue "0.1.2"]]
 
   :main "bot.core")
 
